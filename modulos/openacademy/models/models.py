4124bc63cef4af598ba71964073768a011a6c0dd
Eduardo Palacios <empalacios@ing.usac.edu.gt>
2021-01-06 14:01:15 -0600

    Mostrar el porcentaje de ocupacion de asientos de una sesion.
    Se agrego el campo taken_seats de tipo computed (calculado) para
    conocer el porcentaje de ocupacion de asientos de una sesion, este
    campo depende del numero de asientos (seats) y del numero de asistentes
    (attendee_ids) y se muestra en la vista como una barra de progreso.

--- modulos/openacademy/models/models.py
+++ modulos/openacademy/models/models.py
@@ -24,7 +24,7 @@ class Session(models.Model):
     name = fields.Char(required = True)
     start_date = fields.Date()
     duration = fields.Float(digits = (6, 2), help = 'Duration in days')
-    seats = fields.Integer(string = 'Number ofseats')
+    seats = fields.Integer(string = 'Number of seats')
     instructor_id = fields.Many2one('res.partner', string = 'Instructor',
         domain = [
             '|',
@@ -36,3 +36,12 @@ class Session(models.Model):
         ondelete = 'cascade', string = 'Course', required = True)
     attendee_ids = fields.Many2many('res.partner',
         string = 'Attendees')
+    taken_seats = fields.Float(string='Taken seats', compute = '_taken_seats')
+
+    @api.depends('seats', 'attendee_ids')
+    def _taken_seats(self):
+        for r in self:
+            if not r.seats:
+                r.taken_seats = 0.0
+            else:
+                r.taken_seats = len(r.attendee_ids) * 100.0 / r.seats
