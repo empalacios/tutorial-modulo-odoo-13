670ecd48ef2e28cecb012c0d5de696c81ac6e193
Eduardo Palacios <empalacios@ing.usac.edu.gt>
2021-01-07 11:42:14 -0600

    Agregar dashboard de sesiones: grafica, calendario y lista de cursos.

--- modulos/openacademy/__manifest__.py
+++ modulos/openacademy/__manifest__.py
@@ -21,7 +21,10 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': [
+        'base',
+        'board',
+    ],
 
     # always loaded
     'data': [
@@ -31,6 +34,7 @@
         'views/templates.xml',
         'views/openacademy.xml',
         'views/partner.xml',
+        'views/session_board.xml',
         'reports.xml',
     ],
     # only loaded in demonstration mode
